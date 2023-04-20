2d4c8d11ad789f54c506b041393b56b0368176f0
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-04-20 21:20:56 +0300

    location code description

--- src/components/atoms/Location/Location.tsx
+++ src/components/atoms/Location/Location.tsx
@@ -1,12 +1,23 @@
 import { useAppSelector } from "../../../store/store"
+import weatherCodes from "../../../utils/weatherCodes"
 import styles from "./Location.module.scss"
 
 const Location = () => {
-    const { location } = useAppSelector((store) => store.weather)
+    const { location, weatherData } = useAppSelector((store) => store.weather)
+    let code: number = 0
+
+    if (
+        weatherData.current_weather?.weathercode ||
+        weatherData.current_weather?.weathercode === 0
+    ) {
+        code = weatherData.current_weather?.weathercode
+    } else {
+        code = 100
+    }
 
     return (
         <h1 className={styles.h1}>
-            {location.name} {location.country_code} - Mainly Clear
+            {location.name} {location.country_code} - {weatherCodes[code].description}
         </h1>
     )
 }
