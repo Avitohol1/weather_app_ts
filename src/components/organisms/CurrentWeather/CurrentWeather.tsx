f4e10e4443311c295d67700cba1f34f43075e32b
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-04-20 19:10:21 +0300

    added loader spinner

--- src/components/organisms/CurrentWeather/CurrentWeather.tsx
+++ src/components/organisms/CurrentWeather/CurrentWeather.tsx
@@ -2,10 +2,15 @@ import { useAppSelector } from "../../../store/store"
 import styles from "./CurrentWeather.module.scss"
 import MainWeather from "../../molecules/MainWeather/MainWeather"
 import Details from "../../molecules/Details/Details"
+import Loader from "../../atoms/Loader/Loader"
 
 const CurrentWeather = () => {
-    const { weatherData } = useAppSelector((store) => store.weather)
-    console.log(weatherData)
+    const { weatherData, isLoading } = useAppSelector((store) => store.weather)
+
+    if (isLoading) {
+        return <Loader />
+    }
+
     return (
         <section className={styles.container}>
             {Object.keys(weatherData).length !== 0 ? <MainWeather /> : <></>}
