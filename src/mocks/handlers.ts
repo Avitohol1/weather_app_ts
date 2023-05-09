ee3c899852eaa20c90d49538a72e691e71571c32
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-05-09 18:11:35 +0300

    msw setup

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,22 @@
+import { rest } from "msw"
+import urlParams from "../utils/urlParams"
+import { response } from "./response"
+
+const url = new URL("https://api.open-meteo.com/v1/forecast")
+urlParams.forEach((urlParam) => {
+    const [key, value]: [k: string, v: string] = Object.entries(urlParam)[0]
+    url.searchParams.set(key, value)
+})
+
+const getSearchSuggestions = () => {
+    const url =
+        "https://geocoding-api.open-meteo.com/v1/search?&count=5&language=en&format=json"
+}
+
+const getWeather = () => {
+    return rest.get(url.toString(), (req, res, ctx) => {
+        return res(ctx.json(response))
+    })
+}
+
+export const handlers = [getWeather()]
