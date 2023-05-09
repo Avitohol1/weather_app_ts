30f0121d2c6cf8d1df23e2ce3a03c98db780e7d2
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-05-09 17:35:09 +0300

    vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vite"
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
@@ -6,5 +7,9 @@ export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
         exclude: ["react-pure-carousel"],
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
     },
 })
